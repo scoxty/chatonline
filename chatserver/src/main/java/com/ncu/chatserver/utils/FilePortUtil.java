b8cdd3f3c4f6af99d62fcd49a8c28426eba0c101
scoxty <xty1821901@163.com>
2024-06-08 23:18:55 +0800

    实现单聊模块功能

--- chatserver/src/main/java/com/ncu/chatserver/utils/FilePortUtil.java
+++ chatserver/src/main/java/com/ncu/chatserver/utils/FilePortUtil.java
@@ -1,5 +1,10 @@
 package com.ncu.chatserver.utils;
 
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
 public class FilePortUtil {
+    public static <T> void exportExcel(HttpServletResponse response, String title, String[] headers, List<T> list, List<String> containBean) throws Exception{
+    }
 
 }
